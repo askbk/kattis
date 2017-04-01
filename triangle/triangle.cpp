a3a23910367ba9823a5d89973314f91490c3a237
askbk <askbk@protonmail.com>
2017-04-02 01:10:26 +0200

    mye hyggelig

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main(){
-  int N;
+  int N, TC = 1;
   while (scanf("%d", &N) != EOF) {
-    /* code */
+    long double sum = 2.5;
+    for (size_t i = 1; i < N; i++) {
+      sum = 1 + 1.5*sum;
+    }
+    sum += 3;
+    int digits = 0;
+    while (sum>=1) {
+        sum *= 0.1;
+        digits++;
+    }
+    cout << "Case " << TC << ": " << digits << "\n";
+    ++TC;
   }
 }

f799443d7dc3208b4bf3359ff6e3c4d7e895bce4
askbk <askbk@protonmail.com>
2017-04-01 23:58:57 +0200

    njet

---
+++ shopaholic/shopaholic.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+bool wayToSort(int a, int b){
+  return a>b;
+}
+
+int main(){
+  int N, n, p;
+  long long discount = 0;
+  vector<int> item;
+  cin >> N;
+  n = N;
+  while (n--) {
+    cin >> p;
+    item.push_back(p);
+  }
+  sort(item.begin(), item.end(), wayToSort);
+  for (size_t i = 2; i < N; i+=3) {
+    discount += item[i];
+  }
+  cout << discount << "\n";
+}
---
+++ triangle/triangle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int N;
+  while (scanf("%d", &N) != EOF) {
+    /* code */
+  }
+}

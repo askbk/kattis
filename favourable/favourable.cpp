d2f4d2d18bb4c71695603c7bb20daa2690649687
askbk <askbk@protonmail.com>
2017-04-04 22:16:40 +0200

    TLE siste testcase

---
+++ favourable/favourable.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main(){
+  int T, S;
+  cin >> T;
+  while (T--) {
+    int S, a1, a2, a3, pageNumber;
+    long long favending = 0;
+    string input;
+    queue<int> que;
+    vector< vector< int > > edge;
+    cin >> S;
+    edge.resize(401);
+
+    while (S--) {
+      cin >> pageNumber;
+      cin >> input;
+      if (input == "favourably") {
+        edge[pageNumber].push_back(-1);
+      } else if (input == "catastrophically"){
+        edge[pageNumber].push_back(-2);
+      } else {
+        a1 = stoi(input);
+        cin >> a2 >> a3;
+        edge[pageNumber].push_back(a1); edge[pageNumber].push_back(a2); edge[pageNumber].push_back(a3);
+      }
+    }
+    que.push(1);
+
+    while(!que.empty()){
+      int current = que.front();
+
+      que.pop();
+      int neighbours = edge[current].size();
+      if (neighbours == 1) {
+        if (edge[current][0]==-1) {
+          favending++;
+        }
+      } else {
+        for (size_t i = 0; i < 3; i++) {
+          que.push(edge[current][i]);
+        }
+      }
+
+    }
+    cout << favending << "\n";
+  }
+}

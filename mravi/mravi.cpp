292b585cf74b2d4e16024b2df246f15778e1472d
askbk <askbk@protonmail.com>
2017-04-10 14:05:37 +0200

    nu funker det endelig. f pointere ;)

--- mravi/mravi.cpp
+++ mravi/mravi.cpp
@@ -2,62 +2,54 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <cstdio>
 
 using namespace std;
 
-
-
-struct PipeTo{
-  PipeTo();
-  PipeTo(const double _flow, const int _super):
-    super(_super), flow(_flow) {}
-  int super;
-  double flow;
-};
-
 vector< vector<int> > edge;
-vector<PipeTo> pipe;
-vector<double> required;
+int super[1001];
+double flow[1001];
+double required[1001];
 
 double DFS(int node){
   if (required[node]!=-1) {
     return required[node];
   }
   int children = edge[node].size();
-  vector< pair <double, double> > flowPair;
+  double ans = 0;
   for (size_t i = 0; i < children; i++) {
-    int current = edge[node][current];
-    flowPair.push_back(make_pair(DFS(current), pipe[current].flow));
+    int current = edge[node][i];
+    ans = max(ans, DFS(current)/flow[current]);
   }
-  sort(flowPair.begin(), flowPair.end(), [](auto &left, auto &right) {
-    return left.second*left.first > right.first*right.second;
-});
-  return flowPair[0].first/flowPair[0].second;
+  if (super[node]==1) {
+    return sqrt(ans);
+  } else {
+    return ans;
+  }
 }
 
 int main(){
-  int N, A, B, T, n;
+  int N, A, B, T;
   double X, K;
   cin >> N;
-  edge.resize(N);
-  pipe.reserve(N);
-  required.resize(N);
-  for (size_t i = 0; i < N; i++) {
+  N;
+  edge.resize(N+1);
+  for (size_t i = 1; i < N; i++) {
     cin >> A >> B >> X >> T;
     X *= 0.01;
-    pipe[B-1] = PipeTo(X, T);
-    edge[A-1].push_back(B-1);
+    super[B] = T;
+    flow[B] = X;
+    edge[A].push_back(B);
   }
 
-  for (size_t i = 0; i < N; i++) {
+  for (size_t i = 1; i <= N; i++) {
     cin >> K;
-    if (pipe[i].super==1) {
+    if (K!=-1 && super[i]==1) {
       required[i] = sqrt(K);
     } else {
       required[i] = K;
     }
   }
-
-  cout << DFS(0) << "\n";
+  printf("%.3f\n", DFS(1));
   return 0;
 }

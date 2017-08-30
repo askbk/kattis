d3f185f5b8ff552d83e55126a5b79b876cb925a6
askbk <askbk@protonmail.com>
2017-08-30 23:36:12 +0200

    solved

--- ornaments/ornaments.cpp
+++ ornaments/ornaments.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
-#define PI 3.14159
+#define PI 3.14159265358979
 
 int main(){
+    cout << fixed;
+    cout << setprecision(2);
     for(;;){
         double r,h,s,v;
         cin >> r >> h >> s;
         if(r == 0 && h == 0 && s == 0) return 0;
         s = 1 + s / 100;
         v = acos(r / h);
-        cout << 2 * (sqrt(h * h - r * r) + r * (PI - v)) * s << "\n";
+        cout << 2 * s * (sqrt(h * h - r * r) + r * (PI - v)) << "\n";
     }
 }

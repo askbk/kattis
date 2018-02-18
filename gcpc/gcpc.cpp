1e9e2f4f579b81924db07455e4cd5663c4ba0ac6
askbk <askbk@protonmail.com>
2018-02-18 22:21:21 +0100

    still not solved

--- gcpc/gcpc.cpp
+++ gcpc/gcpc.cpp
@@ -8,14 +8,14 @@ using namespace std;
 int main(){
     int n, m, t, p;
     set< pair<int, int> > ahead; //score, penalty
-    set< pair<int, int> >::iterator it;
+    set< pair<int, int> >::iterator it; //iterator to find stuff in set
 
     cin >> n >> m;
     
     vector< vector < int > > team (n, {0, 0}); //score, penalty
  
     while(m--){
-        cin >> t >> p;
+        cin >> t >> p;  //team, penalty
         --t;
 
         if(t == 0){ //if team 1 solves a problem, increase score and remove everyone whose score is lower from the set
@@ -25,24 +25,29 @@ int main(){
             for(it = ahead.begin(); it != ahead.end();){
                 pair<int, int> curr = *it;
 
-                if(curr.first < team[0][0] || (curr.first == team[0][0] && curr.second <= team[0][1])){
+                if(curr.first < team[0][0] || (curr.first == team[0][0] && curr.second >= team[0][1])){
                     ahead.erase(it++);
                 } else {
                     ++it;
                 }
             }
-        } else { //if someone else solves a problem, first check if they are already ahead. if they are, simply increase their score. else, check if the score increase puts them ahead. if so, add them to the set and increase their score.
+        } else { 
+            //if someone else solves a problem, first check if they are already ahead. 
+            //if they are, simply increase their score. else, check if the score increase 
+            //puts them ahead. if so, add them to the set and increase their score.
+
             int currS = team[t][0], currP = team[t][1];
 
-            if(currS > team[0][0] || (currS == team[0][0] && currP < team[0][1])){
+            if(currS > team[0][0] || (currS == team[0][0] && currP < team[0][1])){  //if they are already ahead
                 ++team[t][0];
                 team[t][1] += p;
-            } else {
-                if(currS + 1 > team[0][0] || (currS + 1 == team[0][0] && currP + p < team[0][1])){
-                    ++team[t][0];
-                    team[t][1] += p;
-                    ahead.insert(make_pair(team[t][0], team[t][1]));
-                }
+            } else if(currS + 1 > team[0][0] || (currS + 1 == team[0][0] && currP + p < team[0][1])){   //if they are ahead after the submission
+                ++team[t][0];
+                team[t][1] += p;
+                ahead.insert(make_pair(team[t][0], team[t][1]));
+            } else {    //if they are behind after the submission
+                ++team[t][0];
+                team[t][1] += p;    
             }
         }
 
